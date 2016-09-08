9bb0fc1a80e6fedf1e998f5134d36155e9f7270b
Robert Stiff <robert.stiff@just-eat.com>
2016-09-08 12:27:36 +0100

    Directory Scanning.

--- index.js
+++ index.js
@@ -3,19 +3,39 @@ var app = express();
 var fs = require('fs');
 var marked = require('marked');
 
-var workingDirectory = process.env.workingDirectory;
+var workingDirectory = process.env.workingDirectory || '/Users/robert.stiff/Dropbox/Notes';
+
+function buildLink(path, filename) {
+    if ( path.endsWith ('/') ) path = path.substr(0, path.length - 1);
+    return '<a href="' + path + '/' + filename+ '">' + filename + '</a>';
+}
 
 app.use(function (req, res, next) {
     var sourceFilePath = workingDirectory + req.originalUrl;
     if ( sourceFilePath.endsWith('/') ) sourceFilePath += 'index.md';
     fs.readFile(sourceFilePath, function(err, data) {
         if ( err ) {
-            res.status = 500;
-            res.send(JSON.stringify(err));
+            if ( err.code === 'EISDIR' || (err.code == 'ENOENT' && sourceFilePath.endsWith('/index.md'))) {
+                if (err.code == 'ENOENT' && sourceFilePath.endsWith('/index.md')) sourceFilePath = sourceFilePath.substr(0, sourceFilePath.length - 'index.md'.length);
+                fs.readdir(sourceFilePath, function(err2, files) {
+                    if (err2) {
+                        res.status = 500;
+                        res.send(JSON.stringify(err2));
+                        next();
+                    } else {
+                        res.send('<h2><a href="..">Back</a></h2></br>' + files.map(buildLink.bind(null, req.originalUrl)).join('<br />'));
+                        next();
+                    }
+                });
+            } else {
+                res.status = 500;
+                res.send(JSON.stringify(err));
+                next();
+            }
         } else { 
             res.send(marked(data.toString('utf8')));
+            next();
         }
-        next();
     });
 });
 
@@ -23,3 +43,4 @@ var port = process.env.PORT || 3000;
 app.listen(port, function(){
     console.log('Listening on port ' + port);
 });
+
