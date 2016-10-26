6a4d71d45e06f0ceec0c66283f3811ecc5ac4c7b
Robert Stiff <uatecuk@gmail.com>
2016-10-26 23:22:21 +0100

    Configurable public folder.

--- serve.js
+++ serve.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.use('/_public/', express.static('public'));
+var public = process.env.public || 'public';
+
+app.use('/_public/', express.static(public));
 app.use('/', express.static('dist'));
 
 var port = process.env.PORT || 3000;
