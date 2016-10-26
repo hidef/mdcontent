d1cf6cde48db9b683a64027dd7f19517c35f9ef1
Robert Stiff <uatecuk@gmail.com>
2016-10-26 23:17:26 +0100

    It's easier to filter files after the fact, and let's not be fancy with working dirs.

--- build.js
+++ build.js
@@ -10,24 +10,19 @@ var viewDir = (process.env.viewDir || '.') + "/views";
 var dots = require("dot").process({ path: viewDir });
 var workingDirectory = process.env.workingDirectory || 'test-content';
 
-function includeFoldersAndMdFiles(fileName, stats)
-{
-    return !fileName.endsWith('.md') && !stats.isDirectory();
-}
-
-recursive(workingDirectory, [includeFoldersAndMdFiles], function (err, files) {
+recursive(workingDirectory, function (err, files) {
     if (err) {
         console.log(err);
         process.exit(-1);
     } else {
-        files = files.filter(function(f) { return f[0] != '.'; });
+        files = files.filter(function(f) { return f.endsWith('.md') && !f.startsWith('node_modules'); });
         files.forEach(f => {
             fs.readFile(f, function (err, data) {
                 if ( err ) {
                     console.log(err);
                     process.exit(-1);
                 } else {
-                    renderFile(f.replace(workingDirectory, ''), data);
+                    renderFile(f, data);
                 }
             });
         });
