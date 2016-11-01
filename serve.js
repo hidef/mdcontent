0b009bad035a7014531adbad10647beb174e1b2e
Robert Stiff <uatecuk@gmail.com>
2016-11-01 23:23:28 +0000

    extension-less URLs.

--- serve.js
+++ serve.js
@@ -4,6 +4,20 @@ var app = express();
 var public = process.env.public || 'public';
 
 app.use('/_public/', express.static(public));
+
+app.use('/', (req, res, next) => {
+    if ( req.url.endsWith('/') ) req.url = req.url + 'index';
+    next();
+});
+
+app.use('/', (req, res, next) => {
+    var parts = req.url.split('/');
+    if ( parts[parts.length - 1].indexOf('.') == -1 ) {
+        req.url = req.url + '.html';
+    }
+    next();
+});
+
 app.use('/', express.static('dist'));
 
 var port = process.env.PORT || 3000;
