bc982c84602a4feeebd10eb2b797cdfe8f4621a7
Robert Stiff <uatecuk@gmail.com>
2016-11-05 17:56:04 +0000

    No implicit any.

--- serve.ts
+++ serve.ts
@@ -7,12 +7,12 @@ var public = process.env.public || 'public';
 
 app.use('/_public/', express.static(public));
 
-app.use('/', (req, res, next) => {
+app.use('/', (req: any, res: any, next: Function) => {
     if ( req.url.endsWith('/') ) req.url = req.url + 'index';
     next();
 });
 
-app.use('/', (req, res, next) => {
+app.use('/', (req: any, res: any, next: Function) => {
     var parts = req.url.split('/');
     if ( parts[parts.length - 1].indexOf('.') == -1 ) {
         req.url = req.url + '.html';
